909cfb324cdb5dfb00ea3cff885afaea563c50ba
eonm <mathis.eon@inist.fr>
2026-03-13 08:58:13 +0000

    Feat : Add public bucket for data ; Call install script from notebook

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip install jupysql duckdb-engine
-mc cp s3/eonm/istex-search-metadata-small.db ./istex-search-metadata-small.db
+mc cp s3/eonm/diffusion/istex-search-metadata-small.db ./istex-search-metadata-small.db
