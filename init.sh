cc53abb1336f93321daa85d4225cb38e52717d73
Mathis EON <mathis.eon@inist.fr>
2026-03-13 11:24:26 +0100

    Improve init script

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-pip install jupysql duckdb-engine
-mc cp s3/eonm/diffusion/istex-search-metadata-small.db ./istex-search-metadata-small.db
+GIT_REPO=ocr-istex
+git clone --depth 1 https://github.com/eonm-pro/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
+
+REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
+mc cp s3/eonm/istex-search-metadata-small.db ./istex-search-metadata-small.db
+
+# Open the relevant notebook when starting Jupyter Lab
+echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/OCR dans Istex.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
